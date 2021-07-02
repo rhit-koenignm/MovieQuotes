376bc9ce20fdf491824a39842f69af5cfbd4c01e
Natalie Koenig <koenignm@rose-hulman.edu>
2021-07-01 22:16:40 -0500

    Initial Commit

---
+++ MovieQuotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieQuotes
+//
+//  Created by Natalie Koenig on 7/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
