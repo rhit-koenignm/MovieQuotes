abbed0d2760adb5c24614b7b56d77ade9d8623f1
Natalie Koenig <koenignm@rose-hulman.edu>
2021-07-07 22:40:29 -0500

    Connected CRUD methods to dialogs

--- MovieQuotes/MovieQuotesTableViewController.swift
+++ MovieQuotes/MovieQuotesTableViewController.swift
@@ -16,15 +16,38 @@ class MovieQuotesTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.leftBarButtonItem = editButtonItem
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(showAddQuoteDialog))
-        
         
         movieQuotes.append(MovieQuote(quote: "Yo Adrian!", movie: "Rocky"))
         movieQuotes.append(MovieQuote(quote: "I'll be back", movie: "The Terminator"))
     }
     
     @objc func showAddQuoteDialog() {
-        print("You pressed the add button")
+        let alertController = UIAlertController(title: "Create a new movie quote", message: "", preferredStyle: .alert)
+        //Configure
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Quote"
+        }
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Movie"
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        let submitAction = UIAlertAction(title: "Create Quote", style: UIAlertAction.Style.default) { (action) in
+            let quoteTextField = alertController.textFields![0] as UITextField
+            let movieTextField = alertController.textFields![1] as UITextField
+            //print(quoteTextField.text)
+            //print(movieTextField.text)
+            let newMovieQuote = MovieQuote(quote: quoteTextField.text!, movie: movieTextField.text!)
+            self.movieQuotes.insert(newMovieQuote, at: 0)
+            self.tableView.reloadData()
+        }
+        alertController.addAction(submitAction)
+        
+        
+        present(alertController, animated: true, completion: nil)
     }
     
     
@@ -38,5 +61,13 @@ class MovieQuotesTableViewController: UITableViewController {
         cell.textLabel?.text = movieQuotes[indexPath.row].quote
         cell.detailTextLabel?.text = movieQuotes[indexPath.row].movie
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == .delete) {
+            movieQuotes.remove(at: indexPath.row)
+            tableView.reloadData()
+            //print("Delete this quote")
+        }
     }
 }
