004fdfed153459d3023f5d1cf6d5ecfadbcf0a3f
Natalie Koenig <koenignm@rose-hulman.edu>
2021-07-26 18:36:20 -0500

    imported rose fire files

--- MovieQuotes/LogInViewController.swift
+++ MovieQuotes/LogInViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase
+import Rosefire
 
 class LogInViewController: UIViewController {
      
@@ -71,4 +72,28 @@ class LogInViewController: UIViewController {
         }
     }
     
+    @IBAction func pressedRosefireLogin(_ sender: Any) {
+        print("TODO: Use Rosefire")
+        
+//        Rosefire.sharedDelegate().uiDelegate = self // This should be your view controller
+//        Rosefire.sharedDelegate().signIn(registryToken: REGISTRY_TOKEN) { (err, result) in
+//          if let err = err {
+//            print("Rosefire sign in error! \(err)")
+//            return
+//          }
+//          print("Result = \(result!.token!)")
+//          print("Result = \(result!.username!)")
+//          print("Result = \(result!.name!)")
+//          print("Result = \(result!.email!)")
+//          print("Result = \(result!.group!)")
+//          Auth.auth().signIn(withCustomToken: result!.token) { (authResult, error) in
+//            if let error = error {
+//              print("Firebase sign in error! \(error)")
+//              return
+//            }
+//            // User is signed in using Firebase!
+//          }
+//        }
+
+    }
 }
