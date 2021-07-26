378707eecf9c7a0ab5ce1f8e5c0c7f60682bc45d
Natalie Koenig <koenignm@rose-hulman.edu>
2021-07-26 00:04:13 -0500

    Automatically remember people that are signed in

--- MovieQuotes/LogInViewController.swift
+++ MovieQuotes/LogInViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class LogInViewController: UIViewController {
      
@@ -15,10 +16,59 @@ class LogInViewController: UIViewController {
     
     let showListSegueIdentifier = "ShowListSegue"
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        emailTextField.placeholder = "Email"
+        passwordTextField.placeholder = "Password"
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if Auth.auth().currentUser != nil {
+            print("Someone is already signed in, move along")
+            self.performSegue(withIdentifier: self.showListSegueIdentifier, sender: self)
+        }
+    }
+    
     @IBAction func pressedSignInNewUser(_ sender: Any) {
+        let email = emailTextField.text!
+        let password = passwordTextField.text!
+        
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                print("Error creating a new user for Email/Password \(error)")
+                return
+            }
+            
+            print("It worked!")
+            print(authResult)
+            print(authResult?.user)
+            print(authResult?.user.email)
+            print(authResult?.user.uid)
+            
+            self.performSegue(withIdentifier: self.showListSegueIdentifier, sender: self)
+        }
+    
     }
     
     @IBAction func pressedLogInExistingUser(_ sender: Any) {
+        let email = emailTextField.text!
+        let password = passwordTextField.text!
+        
+        Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                print("Error logging in an existing user for Email/Password \(error)")
+                return
+            }
+            
+            print("It worked!")
+            print(authResult)
+            print(authResult?.user)
+            print(authResult?.user.email)
+            print(authResult?.user.uid)
+            
+            self.performSegue(withIdentifier: self.showListSegueIdentifier, sender: self)
+        }
     }
     
 }
