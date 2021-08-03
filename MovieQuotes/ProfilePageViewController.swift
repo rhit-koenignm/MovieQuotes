7fd2403492042592901ed914fba58204c3272681
rhit-koenignm <70714241+rhit-koenignm@users.noreply.github.com>
2021-08-02 23:45:00 -0500

    Load image view from Url

--- MovieQuotes/ProfilePageViewController.swift
+++ MovieQuotes/ProfilePageViewController.swift
@@ -21,6 +21,16 @@ class ProfilePageViewController: UIViewController {
         displayNameTextField.addTarget(self, action: #selector(handleNameEdit), for: UIControl.Event.editingChanged)
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        UserManager.shared.beginListening(uid: Auth.auth().currentUser!.uid, changeListener: updateView)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        UserManager.shared.stopListening()
+    }
+    
     @objc func handleNameEdit(){
         if let name = displayNameTextField.text {
             print("Sent the name \(name) to the Firestore!")
@@ -37,6 +47,8 @@ class ProfilePageViewController: UIViewController {
     func updateView() {
         displayNameTextField.text = UserManager.shared.name
         // TODO: Figure out how to load the image for the ImageView asynchronously
-        
+        if UserManager.shared.photoUrl.count > 0 {
+            ImageUtils.load(imageView: profilePhotoImageView, from: UserManager.shared.photoUrl)
+        }
     }
 }
